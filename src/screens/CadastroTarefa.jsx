dfee51577817464a47577c1dcfad89617bf18206
thiagoDimon <othiago.miranda@outlook.com>
2024-06-08 16:29:32 -0300

    Cria opção de finalizar tarefa

--- src/screens/CadastroTarefa.jsx
+++ src/screens/CadastroTarefa.jsx
@@ -1,43 +1,56 @@
-import * as React from 'react';
-import openDB from '../../db';
-import { View, Alert } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import * as React from "react";
+import openDB from "../../db";
+import { View, Alert } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 
 function CadastroTarefa({ navigation }) {
-    const db = openDB();
-    const criarTarefa = async () => {
-        let statement = await db.prepareAsync(`
+  const db = openDB();
+  const criarTarefa = async () => {
+    let statement = await db.prepareAsync(`
             insert into tarefas (descricao, situacao) values ($descricao, $situacao);
             `);
-        try {
-            let result = await statement.executeAsync({ $descricao: text, $situacao: 'P' });
-            if (result.lastInsertRowId !== null) {
-                console.log('tarefa salvou');
-                Alert.alert('Sucesso', 'Tarefa Salva!', [
-                    {
-                        text: 'Ver tarefas',
-                        onPress: () => navigation.navigate('To Do')
-                    }
-                ])
-            }
-        } catch (error) {
-            console.log('Erro ao inserir a tarefa', error);
-        }
+    try {
+      let result = await statement.executeAsync({
+        $descricao: text,
+        $situacao: "P",
+      });
+      if (result.lastInsertRowId !== null) {
+        Alert.alert("Sucesso", "Tarefa Salva!", [
+          {
+            text: "Ver tarefas",
+            onPress: () =>
+              navigation.navigate({
+                name: "To Do",
+                params: { refresh: true },
+              }),
+          },
+        ]);
+      }
+    } catch (error) {
+      console.log("Erro ao inserir a tarefa", error);
     }
-    const [text, setText] = React.useState("");
-    return (
-        <View style={{ flex: 1, gap: 10, padding: 10, justifyContent: 'space-between' }}>
-            <TextInput
-                label="Descrição da Tarefa"
-                value={text}
-                onChangeText={text => setText(text)}
-                mode="outlined"
-            />
-            <Button icon="content-save" mode="contained" onPress={() => criarTarefa()} compact={true}>
-                Salvar
-            </Button>
-        </View>
-    );
+  };
+  const [text, setText] = React.useState("");
+  return (
+    <View
+      style={{ flex: 1, gap: 10, padding: 10, justifyContent: "space-between" }}
+    >
+      <TextInput
+        label="Descrição da Tarefa"
+        value={text}
+        onChangeText={(text) => setText(text)}
+        mode="outlined"
+      />
+      <Button
+        icon="content-save"
+        mode="contained"
+        onPress={() => criarTarefa()}
+        compact={true}
+      >
+        Salvar
+      </Button>
+    </View>
+  );
 }
 
 export default CadastroTarefa;
--- src/screens/ToDo.jsx
+++ src/screens/ToDo.jsx
@@ -1,58 +1,92 @@
-import * as React from 'react';
-import { FlatList, ScrollView, StyleSheet } from 'react-native';
-import { Checkbox, FAB } from 'react-native-paper';
-import openDB from '../../db';
+import * as React from "react";
+import { FlatList, StyleSheet, View, Alert } from "react-native";
+import { Checkbox, FAB } from "react-native-paper";
+import openDB from "../../db";
 
-function Home({ navigation }) {
-    const db = openDB();
-    const [tarefas, setTarefas] = React.useState([]);
+function Home({ route, navigation }) {
+  const db = openDB();
+  const [tarefas, setTarefas] = React.useState([]);
 
-    async function fetchData() {
-        const statement = await db.getAllAsync('select * from tarefas');
-        const tasks = statement;
-        setTarefas(tasks);
+  async function fetchData() {
+    const statement = await db.getAllAsync("select * from tarefas");
+    const tasks = statement;
+    setTarefas(tasks);
+  }
+
+  async function updateDate(tarefa) {
+    const statement = await db.prepareAsync(
+      "update tarefas set situacao = $situacao where id = $id"
+    );
+    const result = await statement.executeAsync({
+      $situacao: tarefa.situacao,
+      $id: tarefa.id,
+    });
+    if (result.lastInsertRowId !== null && tarefa.situcao == "C") {
+      Alert.alert("Sucesso", "Tarefa Finalizada!", [
+        {
+          text: "OK",
+        },
+      ]);
+    } else {
+      Alert.alert("Sucesso", "Tarefa Pendente!", [
+        {
+          text: "OK",
+        },
+      ]);
     }
+  }
+  React.useEffect(() => {
+    fetchData();
+  }, []);
 
-    React.useEffect(() => {
-        const recarregar = navigation.params?.refresh ?? false
-        if (recarregar) {
-            return fetchData()
-        }
-        return fetchData();
-    }, [navigation.params?.refresh])
+  React.useEffect(() => {
+    fetchData();
+  }, [route.params?.refresh]);
 
-    return (
-        <ScrollView style={{ flex: 1 }}>
-            <FlatList
-                data={tarefas}
-                renderItem={({ item, index }) =>
-                    <Checkbox.Item
-                        label={item.descricao}
-                        status={item?.situacao == 'P' ? 'unchecked' : 'checked'}
-                        onPress={() => {
-                            const tempArr = [...tarefas];
-                            tempArr.splice(index, 1, { ...item, situacao: item.situacao == 'P' ? 'C' : 'P' });
-                            setTarefas(tempArr);
-                        }}
-                    />
-                }
-            />
-            <FAB
-                icon="plus"
-                style={styles.fab}
-                onPress={() => navigation.navigate('Cadastrar Tarefa')}
-            />
-        </ScrollView>
-    )
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={tarefas}
+        renderItem={({ item, index }) => (
+          <Checkbox.Item
+            label={item.descricao}
+            status={item?.situacao == "P" ? "unchecked" : "checked"}
+            labelStyle={
+              item?.situacao == "P"
+                ? {}
+                : { textDecorationLine: "line-through" }
+            }
+            onPress={() => {
+              const tempArr = [...tarefas];
+              tempArr.splice(index, 1, {
+                ...item,
+                situacao: item.situacao == "P" ? "C" : "P",
+              });
+              setTarefas(tempArr);
+              updateDate({
+                id: item.id,
+                situacao: item.situacao == "P" ? "C" : "P",
+              });
+            }}
+          />
+        )}
+      />
+      <FAB
+        icon="plus"
+        style={styles.fab}
+        onPress={() => navigation.navigate("Cadastrar Tarefa")}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    fab: {
-        position: 'absolute',
-        margin: 16,
-        right: 0,
-        bottom: 0,
-    },
-})
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default Home;
